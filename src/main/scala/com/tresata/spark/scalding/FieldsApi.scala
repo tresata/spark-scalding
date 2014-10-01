8e0c2d8899d6b38652a46896754923efeda47fbc
Koert Kuipers <koert@tresata.com>
2014-10-01 15:51:51 -0400

    add some more missing methods from RichPipe

--- src/main/scala/com/tresata/spark/scalding/FieldsApi.scala
+++ src/main/scala/com/tresata/spark/scalding/FieldsApi.scala
@@ -3,6 +3,7 @@ package com.tresata.spark.scalding
 import cascading.tuple.{ Tuple => CTuple, TupleEntry, Fields }
 
 import com.twitter.algebird.{ AveragedValue, Semigroup }
+import com.twitter.scalding.Dsl._
 import com.twitter.scalding.{ ReduceOperations, Source, TupleConverter, TupleSetter }
 import com.twitter.scalding.serialization.Externalizer
 
@@ -16,6 +17,7 @@ object FieldsApi {
     def copy: CTuple = new CTuple(ctuple)
     def removeF(declarator: Fields, selector: Fields): CTuple = { ctuple.remove(declarator, selector); ctuple }
     def putF(declarator: Fields, fields: Fields, ctuple: CTuple): CTuple = { this.ctuple.put(declarator, fields, ctuple); this.ctuple }
+    def addF(value: Any): CTuple = { this.ctuple.add(value); this.ctuple }
   }
 }
 
@@ -67,7 +69,7 @@ class FieldsApi(val fields: Fields, val rdd: RDD[CTuple]) extends Serializable {
       case Fields.ALL =>
         new FieldsApi(
           fields.append(fs._2),
-          rdd.map{ ctuple => ctuple.copy.append(f(ctuple)) }
+          rdd.map{ ctuple => ctuple.append(f(ctuple)) }
         )
       case Fields.SWAP =>
         new FieldsApi(
@@ -112,7 +114,7 @@ class FieldsApi(val fields: Fields, val rdd: RDD[CTuple]) extends Serializable {
       case Fields.ALL =>
         new FieldsApi(
           fields.append(fs._2),
-          rdd.flatMap{ ctuple => f(ctuple).map(out => ctuple.copy.append(out)) }
+          rdd.flatMap{ ctuple => f(ctuple).map(out => ctuple.append(out)) }
         )
       case Fields.SWAP =>
         new FieldsApi(
@@ -146,12 +148,20 @@ class FieldsApi(val fields: Fields, val rdd: RDD[CTuple]) extends Serializable {
     )
   }
 
+  def filterNot[A](fields: Fields)(fn: A => Boolean)(implicit conv: TupleConverter[A]): FieldsApi = filter[A](fields)(!fn(_))
+
+  def flatten[T](fs: (Fields, Fields))(implicit conv: TupleConverter[TraversableOnce[T]], setter: TupleSetter[T]): FieldsApi =
+    flatMap[TraversableOnce[T], T](fs)({ it: TraversableOnce[T] => it })(conv, setter)
+
+  def flattenTo[T](fs: (Fields, Fields))(implicit conv: TupleConverter[TraversableOnce[T]], setter: TupleSetter[T]): FieldsApi =
+    flatMapTo[TraversableOnce[T], T](fs)({ it: TraversableOnce[T] => it })(conv, setter)
+
   def insert[A](fields: Fields, value: A)(implicit setter: TupleSetter[A]): FieldsApi = {
     setter.assertArityMatches(fields)
 
     new FieldsApi(
       this.fields.append(fields),
-      rdd.map{ ctuple => new CTuple(ctuple).append(setter(value)) }
+      rdd.map(_.append(setter(value)))
     )
   }
 
@@ -163,7 +173,7 @@ class FieldsApi(val fields: Fields, val rdd: RDD[CTuple]) extends Serializable {
     val pairRDD = rdd.map{ ctuple => (ctuple.get(fields, fs._1), ctuple) }
     val otherPairRDD = other.rdd.map{ ctuple => (ctuple.get(other.fields, fs._2), ctuple) }
     val joinedRDD = if (fs._1 == fs._2)
-      pairRDD.join(otherPairRDD).map{ case (k, (ctuple1, ctuple2)) => val removed = new CTuple(ctuple2); removed.remove(other.fields, fs._2); ctuple1.append(removed) }
+      pairRDD.join(otherPairRDD).map{ case (k, (ctuple1, ctuple2)) => ctuple1.append(ctuple2.copy.removeF(other.fields, fs._2)) }
     else
       pairRDD.join(otherPairRDD).map{ case (k, (ctuple1, ctuple2)) =>  ctuple1.append(ctuple2) }
     new FieldsApi(joinedFields, joinedRDD)
@@ -196,6 +206,43 @@ class FieldsApi(val fields: Fields, val rdd: RDD[CTuple]) extends Serializable {
   def unpivot(fs: (Fields, Fields)): FieldsApi = {
     assert(fs._2.size == 2, "must specify two results")
     flatMap(fs){ te: TupleEntry => TupleConverter.KeyValueList(te) }.discard(fs._1)
+  }
+
+  def name(name: String): FieldsApi = { rdd.setName(name); this }
+
+  def distinct(fields: Fields): FieldsApi = {
+    val tmp = project(fields)
+    new FieldsApi(tmp.fields, tmp.rdd.distinct)
+  }
+
+  def unique(fields: Fields): FieldsApi = distinct(fields)
+
+  def ++(that: FieldsApi): FieldsApi = {
+    require(fields == that.fields, "fields must be same for ++ operation")
+    new FieldsApi(fields, rdd.union(that.rdd))
+  }
+
+  def sample(percent: Double): FieldsApi = new FieldsApi(fields, rdd.sample(false, percent))
+  def sample(percent: Double, seed: Long): FieldsApi = new FieldsApi(fields, rdd.sample(false, percent, seed))
+
+  def sampleWithReplacement(fraction: Double): FieldsApi = new FieldsApi(fields, rdd.sample(true, fraction))
+  def sampleWithReplacement(fraction: Double, seed: Long): FieldsApi = new FieldsApi(fields, rdd.sample(true, fraction, seed))
+
+  def debug: FieldsApi = new FieldsApi(fields, rdd.map{ x => println(x); x })
+
+  // the following are not taken from scalding's RichPipe but from spark's RDD
+
+  def cache(): FieldsApi = { rdd.cache(); this }
+
+  def unpersist(blocking: Boolean = true): FieldsApi = { rdd.unpersist(blocking); this }
+
+  def insertUniqueId(fields: Fields) = {
+    assert(fields.size == 1, "fields must be size 1")
+
+    new FieldsApi(
+      this.fields.append(fields),
+      rdd.zipWithUniqueId.map{ case (ctuple, id) => ctuple.copy.addF(id) }
+    )
   }
 }
 
--- src/test/scala/com/tresata/spark/scalding/FieldsApiSpec.scala
+++ src/test/scala/com/tresata/spark/scalding/FieldsApiSpec.scala
@@ -8,198 +8,200 @@ import org.scalatest.FunSpec
 class FieldsApiSpec extends FunSpec {
   lazy val sc = SparkSuite.sc
 
-  val rdd1 = new FieldsApi(('a, 'b, 'c), sc.parallelize(List(new CTuple("1", "2", "3"), new CTuple("4", "5", "6"))))
-  val rdd2 = new FieldsApi(('a, 'd), sc.parallelize(List(new CTuple("1", "10"), new CTuple("1", "11"))))
-  val rdd3 = new FieldsApi(('d, 'e), sc.parallelize(List(new CTuple("1", "10"), new CTuple("1", "11"))))
+  val fapi1 = new FieldsApi(('a, 'b, 'c), sc.parallelize(List(new CTuple("1", "2", "3"), new CTuple("4", "5", "6"))))
+  val fapi2 = new FieldsApi(('a, 'd), sc.parallelize(List(new CTuple("1", "10"), new CTuple("1", "11"))))
+  val fapi3 = new FieldsApi(('d, 'e), sc.parallelize(List(new CTuple("1", "10"), new CTuple("1", "11"))))
+  val fapi4 = new FieldsApi(('x, 'y), sc.parallelize(List(new CTuple("1", "2"), new CTuple("1", "3"), new CTuple("1", "2"))))
+  val fapi5 = new FieldsApi(('x, 'y), sc.parallelize(List(new CTuple("2", "3"))))
 
   describe("A FieldsApi") {
     it("should rename") {
-      assert(rdd1.rename('a -> 'd).fields == (('d, 'b, 'c): Fields))
+      assert(fapi1.rename('a -> 'd).fields === (('d, 'b, 'c): Fields))
     }
 
     it("should not allow duplicate fields in rename") {
-      intercept[IllegalArgumentException]{ rdd1.rename('a -> 'c) }
+      intercept[IllegalArgumentException]{ fapi1.rename('a -> 'c) }
     }
 
     it("should not allow all fields in rename") {
-      intercept[TupleException]{ rdd1.rename('* -> 'c) }
-      intercept[TupleException]{ rdd1.rename('c -> '*) }
-      intercept[TupleException]{ rdd1.rename('* -> '*) }
+      intercept[TupleException]{ fapi1.rename('* -> 'c) }
+      intercept[TupleException]{ fapi1.rename('c -> '*) }
+      intercept[TupleException]{ fapi1.rename('* -> '*) }
     }
 
     it("should discard") {
-      val tmp = rdd1.discard('a, 'c)
-      assert(tmp.fields == ('b: Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("2"), new CTuple("5")))
+      val tmp = fapi1.discard('a, 'c)
+      assert(tmp.fields === ('b: Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("2"), new CTuple("5")))
     }
 
     it("should map replace") {
-      val tmp = rdd1.map('a -> 'a){ (x: String) => "7" }
-      assert(tmp.fields == (('a, 'b, 'c): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("7", "2", "3"), new CTuple("7", "5", "6")))
+      val tmp = fapi1.map('a -> 'a){ (x: String) => "7" }
+      assert(tmp.fields === (('a, 'b, 'c): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("7", "2", "3"), new CTuple("7", "5", "6")))
     }
 
     it("should map replace to args") {
-      val tmp = rdd1.map('a -> Fields.ARGS){ (x: String) => "7" }
-      assert(tmp.fields == (('a, 'b, 'c): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("7", "2", "3"), new CTuple("7", "5", "6")))
+      val tmp = fapi1.map('a -> Fields.ARGS){ (x: String) => "7" }
+      assert(tmp.fields === (('a, 'b, 'c): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("7", "2", "3"), new CTuple("7", "5", "6")))
     }
 
     it("should map append") {
-      val tmp = rdd1.map('a -> 'd){ (x: String) => "7" }
-      assert(tmp.fields == (('a, 'b, 'c, 'd): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("1", "2", "3", "7"), new CTuple("4", "5", "6", "7")))
+      val tmp = fapi1.map('a -> 'd){ (x: String) => "7" }
+      assert(tmp.fields === (('a, 'b, 'c, 'd): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("1", "2", "3", "7"), new CTuple("4", "5", "6", "7")))
     }
 
     it("should map swap") {
-      val tmp = rdd1.map('a -> ('a, 'd)){ (x: String) => ("7", "8") }
-      assert(tmp.fields == (('b, 'c, 'a, 'd): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("2", "3", "7", "8"), new CTuple("5", "6", "7", "8")))
+      val tmp = fapi1.map('a -> ('a, 'd)){ (x: String) => ("7", "8") }
+      assert(tmp.fields === (('b, 'c, 'a, 'd): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("2", "3", "7", "8"), new CTuple("5", "6", "7", "8")))
     }
 
     it("should map all") {
-      val tmp = rdd1.map('* -> 'x){ (ctuple: CTuple) => "8" }
-      assert(tmp.fields == (('a, 'b, 'c, 'x): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("1", "2", "3", "8"), new CTuple("4", "5", "6", "8")))
+      val tmp = fapi1.map('* -> 'x){ (ctuple: CTuple) => "8" }
+      assert(tmp.fields === (('a, 'b, 'c, 'x): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("1", "2", "3", "8"), new CTuple("4", "5", "6", "8")))
     }
 
     it("should map all to all") {
-      val tmp = rdd1.map('* -> '*){ (ctuple: CTuple) => new CTuple("0", "0", "0") }
-      assert(tmp.fields == (('a, 'b, 'c): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("0", "0", "0"), new CTuple("0", "0", "0")))
+      val tmp = fapi1.map('* -> '*){ (ctuple: CTuple) => new CTuple("0", "0", "0") }
+      assert(tmp.fields === (('a, 'b, 'c): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("0", "0", "0"), new CTuple("0", "0", "0")))
     }
 
     it("should map all to args") {
-      val tmp = rdd1.map('* -> Fields.ARGS){ (ctuple: CTuple) => new CTuple("0", "0", "0") }
-      assert(tmp.fields == (('a, 'b, 'c): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("0", "0", "0"), new CTuple("0", "0", "0")))
+      val tmp = fapi1.map('* -> Fields.ARGS){ (ctuple: CTuple) => new CTuple("0", "0", "0") }
+      assert(tmp.fields === (('a, 'b, 'c): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("0", "0", "0"), new CTuple("0", "0", "0")))
     }
 
     it("should mapTo") {
-      val tmp = rdd1.mapTo('a -> ('a, 'd)){ (x: String) => ("7", "8") }
-      assert(tmp.fields == (('a, 'd): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("7", "8"), new CTuple("7", "8")))
+      val tmp = fapi1.mapTo('a -> ('a, 'd)){ (x: String) => ("7", "8") }
+      assert(tmp.fields === (('a, 'd): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("7", "8"), new CTuple("7", "8")))
     }
 
     it("should mapTo all to something") {
-      val tmp = rdd1.mapTo('* -> ('a, 'd)){ (ctuple: CTuple) => ("7", "8") }
-      assert(tmp.fields == (('a, 'd): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("7", "8"), new CTuple("7", "8")))
+      val tmp = fapi1.mapTo('* -> ('a, 'd)){ (ctuple: CTuple) => ("7", "8") }
+      assert(tmp.fields === (('a, 'd): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("7", "8"), new CTuple("7", "8")))
     }
 
     it("should flatMap replace") {
-      val tmp = rdd1.flatMap('a -> 'a){ (x: String) => List("7", "8") }
-      assert(tmp.fields == (('a, 'b, 'c): Fields))
-      assert(tmp.rdd.collect.toList == List(
+      val tmp = fapi1.flatMap('a -> 'a){ (x: String) => List("7", "8") }
+      assert(tmp.fields === (('a, 'b, 'c): Fields))
+      assert(tmp.rdd.collect.toList === List(
         new CTuple("7", "2", "3"), new CTuple("8", "2", "3"),
         new CTuple("7", "5", "6"), new CTuple("8", "5", "6")
       ))
     }
 
     it("should flatMap replace to args") {
-      val tmp = rdd1.flatMap('a -> Fields.ARGS){ (x: String) => List("7", "8") }
-      assert(tmp.fields == (('a, 'b, 'c): Fields))
-      assert(tmp.rdd.collect.toList == List(
+      val tmp = fapi1.flatMap('a -> Fields.ARGS){ (x: String) => List("7", "8") }
+      assert(tmp.fields === (('a, 'b, 'c): Fields))
+      assert(tmp.rdd.collect.toList === List(
         new CTuple("7", "2", "3"), new CTuple("8", "2", "3"),
         new CTuple("7", "5", "6"), new CTuple("8", "5", "6")
       ))
     }
 
     it("should flatMap append") {
-      val tmp = rdd1.flatMap('a -> 'd){ (x: String) => List("7", "8") }
-      assert(tmp.fields == (('a, 'b, 'c, 'd): Fields))
-      assert(tmp.rdd.collect.toList == List(
+      val tmp = fapi1.flatMap('a -> 'd){ (x: String) => List("7", "8") }
+      assert(tmp.fields === (('a, 'b, 'c, 'd): Fields))
+      assert(tmp.rdd.collect.toList === List(
         new CTuple("1", "2", "3", "7"), new CTuple("1", "2", "3", "8"),
         new CTuple("4", "5", "6", "7"), new CTuple("4", "5", "6", "8"))
       )
     }
 
     it("should flatMap swap") {
-      val tmp = rdd1.flatMap('a -> ('a, 'd)){ (x: String) => List(("7", "8"), ("9", "10")) }
-      assert(tmp.fields == (('b, 'c, 'a, 'd): Fields))
-      assert(tmp.rdd.collect.toList == List(
+      val tmp = fapi1.flatMap('a -> ('a, 'd)){ (x: String) => List(("7", "8"), ("9", "10")) }
+      assert(tmp.fields === (('b, 'c, 'a, 'd): Fields))
+      assert(tmp.rdd.collect.toList === List(
         new CTuple("2", "3", "7", "8"), new CTuple("2", "3", "9", "10"),
         new CTuple("5", "6", "7", "8"), new CTuple("5", "6", "9", "10")
       ))
     }
 
     it("should flatMap all") {
-      val tmp = rdd1.flatMap('* -> 'x){ (ctuple: CTuple) => List("8", "9") }
-      assert(tmp.fields == (('a, 'b, 'c, 'x): Fields))
-      assert(tmp.rdd.collect.toList == List(
+      val tmp = fapi1.flatMap('* -> 'x){ (ctuple: CTuple) => List("8", "9") }
+      assert(tmp.fields === (('a, 'b, 'c, 'x): Fields))
+      assert(tmp.rdd.collect.toList === List(
         new CTuple("1", "2", "3", "8"), new CTuple("1", "2", "3", "9"),
         new CTuple("4", "5", "6", "8"), new CTuple("4", "5", "6", "9")
       ))
     }
 
     it("should flatMap all to all") {
-      val tmp = rdd1.flatMap('* -> '*){ (ctuple: CTuple) => List(new CTuple("0", "0", "0"), new CTuple("1", "1", "1")) }
-      assert(tmp.fields == (('a, 'b, 'c): Fields))
-      assert(tmp.rdd.collect.toList == List(
+      val tmp = fapi1.flatMap('* -> '*){ (ctuple: CTuple) => List(new CTuple("0", "0", "0"), new CTuple("1", "1", "1")) }
+      assert(tmp.fields === (('a, 'b, 'c): Fields))
+      assert(tmp.rdd.collect.toList === List(
         new CTuple("0", "0", "0"), new CTuple("1", "1", "1"),
         new CTuple("0", "0", "0"), new CTuple("1", "1", "1")
       ))
     }
 
     it("should flatMap all to args") {
-      val tmp = rdd1.flatMap('* -> Fields.ARGS){ (ctuple: CTuple) => List(new CTuple("0", "0", "0"), new CTuple("1", "1", "1")) }
-      assert(tmp.fields == (('a, 'b, 'c): Fields))
-      assert(tmp.rdd.collect.toList == List(
+      val tmp = fapi1.flatMap('* -> Fields.ARGS){ (ctuple: CTuple) => List(new CTuple("0", "0", "0"), new CTuple("1", "1", "1")) }
+      assert(tmp.fields === (('a, 'b, 'c): Fields))
+      assert(tmp.rdd.collect.toList === List(
         new CTuple("0", "0", "0"), new CTuple("1", "1", "1"),
         new CTuple("0", "0", "0"), new CTuple("1", "1", "1")
       ))
     }
 
     it("should flatMapTo") {
-      val tmp = rdd1.flatMapTo('a -> ('a, 'd)){ (x: String) => List(("7", "8"), ("9", "10")) }
-      assert(tmp.fields == (('a, 'd): Fields))
-      assert(tmp.rdd.collect.toList == List(
+      val tmp = fapi1.flatMapTo('a -> ('a, 'd)){ (x: String) => List(("7", "8"), ("9", "10")) }
+      assert(tmp.fields === (('a, 'd): Fields))
+      assert(tmp.rdd.collect.toList === List(
         new CTuple("7", "8"), new CTuple("9", "10"),
         new CTuple("7", "8"), new CTuple("9", "10")
       ))
     }
 
     it("should flatMapTo all to something") {
-      val tmp = rdd1.flatMapTo('* -> ('a, 'd)){ (ctuple: CTuple) => List(("7", "8"), ("9", "10")) }
-      assert(tmp.fields == (('a, 'd): Fields))
-      assert(tmp.rdd.collect.toList == List(
+      val tmp = fapi1.flatMapTo('* -> ('a, 'd)){ (ctuple: CTuple) => List(("7", "8"), ("9", "10")) }
+      assert(tmp.fields === (('a, 'd): Fields))
+      assert(tmp.rdd.collect.toList === List(
         new CTuple("7", "8"), new CTuple("9", "10"),
         new CTuple("7", "8"), new CTuple("9", "10")
       ))
     }
 
     it("should filter") {
-      val tmp = rdd1.filter('a){ a: String => a == "1" }
-      assert(tmp.fields == (('a, 'b, 'c): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("1", "2", "3")))
+      val tmp = fapi1.filter('a){ a: String => a == "1" }
+      assert(tmp.fields === (('a, 'b, 'c): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("1", "2", "3")))
     }
 
     it("should insert") {
-      val tmp = rdd1.insert('d, 1: JInt)
-      assert(tmp.fields == (('a, 'b, 'c, 'd): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("1", "2", "3", 1: JInt), new CTuple("4", "5", "6", 1: JInt)))
+      val tmp = fapi1.insert('d, 1: JInt)
+      assert(tmp.fields === (('a, 'b, 'c, 'd): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("1", "2", "3", 1: JInt), new CTuple("4", "5", "6", 1: JInt)))
     }
 
     it("should join with same keys") {
-      val tmp = rdd1.join('a, rdd2)
-      assert(tmp.fields == (('a, 'b, 'c, 'd): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("1", "2", "3", "10"), new CTuple("1", "2", "3", "11")))
+      val tmp = fapi1.join('a, fapi2)
+      assert(tmp.fields === (('a, 'b, 'c, 'd): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("1", "2", "3", "10"), new CTuple("1", "2", "3", "11")))
     }
 
     it("should join with different keys") {
-      val tmp = rdd1.join('a -> 'd, rdd3)
-      assert(tmp.fields == (('a, 'b, 'c, 'd, 'e): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("1", "2", "3", "1", "10"), new CTuple("1", "2", "3", "1", "11")))
+      val tmp = fapi1.join('a -> 'd, fapi3)
+      assert(tmp.fields === (('a, 'b, 'c, 'd, 'e): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("1", "2", "3", "1", "10"), new CTuple("1", "2", "3", "1", "11")))
     }
 
     it("should left join") {
-      val tmp = rdd1.leftJoin('a -> 'd, rdd3)
-      assert(tmp.fields == (('a, 'b, 'c, 'd, 'e): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("1", "2", "3", "1", "10"), new CTuple("1", "2", "3", "1", "11"), new CTuple("4", "5", "6", null, null)))
+      val tmp = fapi1.leftJoin('a -> 'd, fapi3)
+      assert(tmp.fields === (('a, 'b, 'c, 'd, 'e): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("1", "2", "3", "1", "10"), new CTuple("1", "2", "3", "1", "11"), new CTuple("4", "5", "6", null, null)))
     }
 
     it("should groupBy") {
-      val tmp = rdd1
+      val tmp = fapi1
         .insert('g, "group1")
         .groupBy('g)(_
           .average('a -> 'avgA)
@@ -208,20 +210,59 @@ class FieldsApiSpec extends FunSpec {
         )
         .map('avgA -> 'avgA)(identity[String])
         .map('size -> 'size)(identity[String])
-      assert(tmp.fields == (('g, 'avgA, 'size, 'setB): Fields))
-      assert(tmp.rdd.collect.toList == List(new CTuple("group1", "2.5", "2", "2,5")))
+      assert(tmp.fields === (('g, 'avgA, 'size, 'setB): Fields))
+      assert(tmp.rdd.collect.toList === List(new CTuple("group1", "2.5", "2", "2,5")))
     }
   }
 
   it("should support unpivot and pivot") {
-    val tmp1 = rdd1.unpivot(('b, 'c) -> ('key, 'value))
-    assert(tmp1.fields == (('a, 'key, 'value): Fields))
-    assert(tmp1.rdd.collect.toList == List(
+    val tmp1 = fapi1.unpivot(('b, 'c) -> ('key, 'value))
+    assert(tmp1.fields === (('a, 'key, 'value): Fields))
+    assert(tmp1.rdd.collect.toList === List(
       new CTuple("1", "b", "2"), new CTuple("1", "c", "3"),
       new CTuple("4", "b", "5"), new CTuple("4", "c", "6")
     ))
     val tmp2 = tmp1.groupBy('a)(_.pivot(('key, 'value) -> ('b, 'c)))
-    assert(tmp2.fields == (('a, 'b, 'c): Fields))
-    assert(tmp2.rdd.collect.toList == List(new CTuple("1", "2", "3"), new CTuple("4", "5", "6")))
+    assert(tmp2.fields === (('a, 'b, 'c): Fields))
+    assert(tmp2.rdd.collect.toList === List(new CTuple("1", "2", "3"), new CTuple("4", "5", "6")))
+  }
+
+  it("should dedupe") {
+    val tmp1 = fapi4.unique('*)
+    assert(tmp1.fields === (('x, 'y): Fields))
+    assert(tmp1.rdd.collect.toList === List(new CTuple("1", "2"), new CTuple("1", "3")))
+
+    val tmp2 = fapi4.unique('x)
+    assert(tmp2.fields === ('x: Fields))
+    assert(tmp2.rdd.collect.toList === List(new CTuple("1")))
+  }
+
+  it("should merge") {
+    val tmp1 = fapi4 ++ fapi5
+    assert(tmp1.fields === (('x, 'y): Fields))
+    assert(tmp1.rdd.collect.toList === List(new CTuple("1", "2"), new CTuple("1", "3"), new CTuple("1", "2"), new CTuple("2", "3")))
+  }
+
+  it("should sample") {
+    val tmp1 = new FieldsApi(('a), sc.parallelize((1 to 1000).map{ i => new CTuple(i: JInt) }))
+    val tmp2 = tmp1.sample(0.1, 1L)
+    val array = tmp2.rdd.collect.map(_.getInteger(0))
+    assert(array.size > 80 && array.size < 120)
+    assert(array.toSet.size === array.size)
+  }
+
+  it("should sample with replacement") {
+    val tmp1 = new FieldsApi(('a), sc.parallelize((1 to 1000).map{ i => new CTuple(i: JInt) }))
+    val tmp2 = tmp1.sampleWithReplacement(0.1, 1L)
+    val array = tmp2.rdd.collect.map(_.getInteger(0))
+    assert(array.size > 80 && array.size < 120)
+    assert(array.toSet.size < array.size)
+  }
+
+  it("should insert unique ids") {
+    val tmp1 = new FieldsApi(('a), sc.parallelize((1 to 1000).map{ i => new CTuple(i: JInt) }))
+    val tmp2 = tmp1.insertUniqueId('id)
+    assert(tmp2.fields === (('a, 'id): Fields))
+    assert(tmp2.project('id).rdd.collect.toSet.size === 1000)
   }
 }
