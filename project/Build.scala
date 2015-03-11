bbaa70a5765ed786c7020e65737913eef90fe5cc
Koert Kuipers <koert@tresata.com>
2015-03-11 16:10:33 -0400

    publish to sonatype OSSRH

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,36 @@ object ProjectBuild extends Build {
     resolvers += "Concurrent Maven Repo" at "http://conjars.org/repo",
     publishMavenStyle := true,
     pomIncludeRepository := { x => false },
-    publishArtifact in Test := false
+    publishTo := {
+      val nexus = "https://oss.sonatype.org/"
+      if (isSnapshot.value)
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    credentials += Credentials(Path.userHome / ".m2" / "credentials_sonatype"),
+    publishArtifact in Test := false,
+    pomExtra := (
+      <url>https://github.com/tresata/spark-scalding</url>
+      <licenses>
+        <license>
+          <name>Apache 2</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>  
+          <distribution>repo</distribution>
+          <comments>A business-friendly OSS license</comments>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:tresata/spark-scalding.git</url>
+        <connection>scm:git:git@github.com:tresata/spark-scalding.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>koertkuipers</id>
+          <name>Koert Kuipers</name>
+          <url>https://github.com/koertkuipers</url>
+        </developer>
+      </developers>)
   )
 
   lazy val project = Project(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,3 +2,4 @@ addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
 
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.12.0")
 
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
