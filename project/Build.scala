099f69eb50c229c974a04be35d606f2564a8e5b7
Koert Kuipers <koert@tresata.com>
2015-05-26 23:25:31 -0400

    switch to spark-sorted release

--- project/Build.scala
+++ project/Build.scala
@@ -59,7 +59,7 @@ object ProjectBuild extends Build {
           exclude("com.twitter", "chill-java")
           exclude("com.twitter", "chill_2.10")
           exclude("com.twitter", "chill_2.11"),
-        "com.tresata" %% "spark-sorted" % "0.2.0-SNAPSHOT",
+        "com.tresata" %% "spark-sorted" % "0.2.0",
         "org.apache.spark" %% "spark-core" % "1.3.0" % "provided",
         "org.slf4j" % "slf4j-log4j12" % "1.7.5" % "test",
         "org.scalatest" %% "scalatest" % "2.2.1" % "test"
